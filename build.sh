9998ed225ec30bdc343350848b2ded958ff77b16
Thomas Sarlandie <thomas@sarlandie.net>
2019-10-07 13:39:03 -0700

    feature: support passing project/config via environment

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Docker Container Building $1:$2"
+# Support passing project / context as environment variable or run parameters
+PROJECT=${PROJECT:-$1}
+CONF=${CONF:-$2}
+
+echo "Docker Container Building $PROJECT:$CONF"
 
 set -x -e
 
-/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
-make -C $1 CONF=$2 build || exit 2
+/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $PROJECT@$CONF || exit 1
+make -C $PROJECT CONF=$CONF build || exit 2
